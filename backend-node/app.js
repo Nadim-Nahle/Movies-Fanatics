244aba44713d49f7b5559d2c707b31ee7ea8b163
Nadim <nahlenadim@gmail.com>
2022-06-27 14:19:02 +0300

    Backend-Node Create- db connection

---
+++ backend-node/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app= express();
+const mongoose = require('mongoose');
+require('dotenv/config');
+const bodyParser = require('body-parser');
+
+const DB_CONNECTION = process.env.DB_CONNECTION;
+
+app.use(bodyParser.json());
+
+//DB CONNECTION
+mongoose.connect(DB_CONNECTION, () => {
+    try{
+        console.log('db connected');
+    }
+    catch(error){
+        console.log('error');
+    }
+})
+
+
+app.use(express.json());
+app.listen(5000);
