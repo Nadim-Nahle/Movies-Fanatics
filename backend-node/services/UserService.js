21bb9b2075d84ccef9d41c7590a002c8fa70c194
Nadim <nahlenadim@gmail.com>
2022-06-27 14:40:49 +0300

    Backend-Node Create- User Service

---
+++ backend-node/services/UserService.js
@@ -0,0 +1,18 @@
+const User = require('../models/user')
+
+//REGISTER SERVICE
+async function addUser(body, hashPassword){
+    const {
+        name,
+        email,
+
+    } = body;
+
+    const user = new User({
+        name,
+        email,
+        password: hashPassword
+    })
+    return await user.save();
+}
+
