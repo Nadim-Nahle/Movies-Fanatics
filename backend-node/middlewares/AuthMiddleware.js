a93ace5f27a5fde64d151ad8952a140073cc2c00
Nadim <nahlenadim@gmail.com>
2022-06-29 09:39:25 +0300

    Backend-Node Create- Auth Middleware

---
+++ backend-node/middlewares/AuthMiddleware.js
@@ -0,0 +1,32 @@
+const jwt = require('jsonwebtoken')
+const Users = require('../models/user');
+const TOKEN_SECRET = process.env.TOKEN_SECRET;
+
+//AUTH MIDDLEWARE
+const auth = async (req,res,next) =>{
+   try{
+       const token = req.header('Authorization').replace('Bearer ', '');
+       //console.log(token);
+       
+       const decode = await jwt.verify(token, TOKEN_SECRET);
+       
+       const user = await Users.findOne({ _id: decode._id, 'tokens.token': token })
+       //console.log(user);
+       
+       
+       if(!user)
+        throw new Error()
+    req.token = token
+    
+    req.user = user;
+    //console.log('nice')
+    //console.log(user);
+    //console.log('nice')
+    next();
+   }catch(e){
+       res.status(400).send({error:"authentication X"})
+   }
+}
+
+//EXPORTING MODULE
+module.exports = auth;
