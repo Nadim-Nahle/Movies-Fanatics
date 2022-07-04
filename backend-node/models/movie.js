de4c6f513ee4bc640c9cfb1eede0f8b89cd87dc4
Nadim <nahlenadim@gmail.com>
2022-07-04 09:15:24 +0300

    Backend-Node Create- Movie Model

--- backend-node/models/movie.js
+++ backend-node/models/movie.js
@@ -6,39 +6,47 @@ const userSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
-    min: 6,
+    min: 2,
     max: 255,
+    unique: true,
   },
-  email: {
+
+  description: {
     type: String,
     required: true,
     min: 6,
     max: 255,
     unique: true,
   },
-  password: {
-    type: String,
-    required: true,
-    min: [6, 'please choose a stronger password'],
-    max: 1024,
-  },
+
   date: {
     type: Date,
     default: Date.now,
   },
 
-  roles: {
-    type: [{
-        type: String,
-        enum: ['user', 'admin']
-    }],
-    default: ['user']
-},
+  img: {
+    type: String,
+  },
 
-isPremium:{
-  type: Boolean,
-  default: false
-}
+  video: {
+    type: String,
+  },
+
+  year: {
+    type: String,
+  },
+
+  genre: {
+    type: String,
+  },
+
+  isSeries: {
+    type: Boolean,
+    default: false,
+  },
+  
+
+  
   
 
 });
