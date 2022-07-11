a379129370d34dcf3fc19b62a7d7b7dc5a20d971
Nadim <nahlenadim@gmail.com>
2022-07-11 18:07:03 +0300

    Frontend-React Add- Routes

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -4,12 +4,23 @@ import Navbar from "./components/navbar/Navbar";
 import Movies from "./pages/movies/Movies";
 import Register from "./pages/register/Register";
 import Login from "./pages/login/Login";
+import { Route, Routes } from "react-router-dom";
+import Layout from "./components/Layout";
 
 function App() {
   return (
     <div>
-      <Navbar />
-      <Homepage />
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          {/*PUBLIC ROUTES*/}
+          <Route path="/" element={<Homepage />}></Route>
+          <Route path="/register" element={<Register />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/movies" element={<Movies />}></Route>
+        </Route>
+        
+      </Routes>
+
     </div>
   );
 }
