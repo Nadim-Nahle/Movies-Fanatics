c0c15f43f87745eb9f7e179255e297860ef5a895
Nadim <nahlenadim@gmail.com>
2022-07-12 07:59:50 +0300

    Frontend-React Create- Auth Actions

--- frontend-react/src/index.js
+++ frontend-react/src/index.js
@@ -4,14 +4,17 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { AuthProvider } from './context/AuthProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>   
+    <BrowserRouter> 
+      <AuthProvider>
         <Routes>
           <Route path="/*" element={<App />} />
         </Routes>
+      </AuthProvider>  
     </BrowserRouter>
   </React.StrictMode>
 );
