b97562d61710f240759c65d9f0c6f7ab1496b9bc
Nadim <nahlenadim@gmail.com>
2022-07-13 08:55:50 +0300

    Frontend-React Create- Crad container

--- frontend-react/src/pages/swipe/Swipe.jsx
+++ frontend-react/src/pages/swipe/Swipe.jsx
@@ -5,23 +5,23 @@ import './swipe.css';
 const db = [
     {
       name: 'Richard Hendricks',
-      url: './img/richard.jpg'
+      url: 'https:/i.imgur.com/oPj4A8u.jpg'
     },
     {
       name: 'Erlich Bachman',
-      url: './img/erlich.jpg'
+      url: 'https:/i.imgur.com/oPj4A8u.jpg'
     },
     {
       name: 'Monica Hall',
-      url: './img/monica.jpg'
+      url: 'https:/i.imgur.com/oPj4A8u.jpg'
     },
     {
       name: 'Jared Dunn',
-      url: './img/jared.jpg'
+      url: 'https:/i.imgur.com/oPj4A8u.jpg'
     },
     {
       name: 'Dinesh Chugtai',
-      url: './img/dinesh.jpg'
+      url: 'https:/i.imgur.com/oPj4A8u.jpg'
     }
   ]
 
@@ -41,16 +41,22 @@ const Swipe = () => {
 
   return (
     <div className='dashboard'>
-        <div className="swiper-container">
+        <div className="swipe-container">
             <div className="card-container">
 
             {characters.map((character) =>
             <TinderCard className='swipe' key={character.name} onSwipe={(dir) => swiped(dir, character.name)} onCardLeftScreen={() => outOfFrame(character.name)}>
-                <div style={{ backgroundImage: 'url(' + character.url + ')' }} className='card'>
+                <div style={{ backgroundImage: 'url(' + character.url + ')' }} 
+                className='card'
+                >
                     <h3>{character.name}</h3>
                 </div>
           </TinderCard>
         )}
+        <div className='swipe-info'>
+                {lastDirection ? <p> You swiped {lastDirection}</p> : <p/>}
+        </div>
+
             </div>
         </div>
     </div>
