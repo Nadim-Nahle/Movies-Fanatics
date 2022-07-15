44b59f38efb6fca58d7190d2fd6ce319cd0fc7fd
Nadim <nahlenadim@gmail.com>
2022-07-15 09:39:55 +0300

    Socket Create- addUser fuctio

--- socket/index.js
+++ socket/index.js
@@ -4,6 +4,19 @@ const io = require('socket.io')(8900,{
    },
 });
 
+let users = [];
+
+const addUser = (userId,socketId) =>{
+    !users.some(user=>user.userId === userId) &&
+    users.push({ userId, socketId});
+};
+
+
 io.on('connection', (socket) => {
     console.log('a user connected.')
-})
+    //take userId and socketId from user
+    socket.on('addUser', userId=>{
+        addUser(userId, socket.id);
+        io.emit('getUsers', users);
+    });
+});
