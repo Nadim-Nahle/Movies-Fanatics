9d7232855c6a4c26de56a7e4b8418a918424da53
Nadim <nahlenadim@gmail.com>
2022-07-15 09:59:09 +0300

    Socket create- GetUser function

--- socket/index.js
+++ socket/index.js
@@ -15,6 +15,10 @@ const removeUser = (socketId) =>{
     users = users.filter(user=>user.socketId !== socketId )
 }
 
+const getUser = (userId) => {
+    return users.find((user) => user.userId === userId);
+  };
+
 io.on('connection', (socket) => {
     console.log('a user connected.')
     //take userId and socketId from user
