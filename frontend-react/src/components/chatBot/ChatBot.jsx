b3a2d294f5ed64e45630b8c628b04949506ff282
Nadim <nahlenadim@gmail.com>
2022-07-16 08:21:28 +0300

    Frontend-React Add- chatbot components

--- frontend-react/src/components/chatBot/ChatBot.jsx
+++ frontend-react/src/components/chatBot/ChatBot.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
-import './chatbot.css'
-import botImage from '../../img/bot_image.jpg'
+import React, { useState } from 'react';
+import './chatbot.css';
+import botImage from '../../img/bot_image.jpg';
 
 const ChatBot = () => {
+
+    const date = new Date();
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds();
+    const day = date.getDay();
+    const month = date.getMonth();
+    const year = date.getFullYear();
+
+    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    const [time, setTime] = useState(`${hours}:${minutes}:${seconds}`); //using the useState hook to get the data from the local time and set it to the time variable
+    const [dateTime, setDateTime] = useState(`${days[day]}, ${day} ${months[month]} ${year}`); //using the useState hook to get the data from the local date and set it to the dateTime variable
+
   return (
-    <div className="chatbot">
+    <div className="chatbot" >
       <div className="chatbot-wrapper">
         <div className="chatbot-content">
           <div className="chatbot-header">
