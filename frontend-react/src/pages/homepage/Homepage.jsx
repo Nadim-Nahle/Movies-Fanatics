9c9d5ca30f29b08f5b483b1b8f19197c23b2ba63
Nadim <nahlenadim@gmail.com>
2022-07-21 10:15:38 +0300

    Frontend-React Add- Background

--- frontend-react/src/pages/homepage/Homepage.jsx
+++ frontend-react/src/pages/homepage/Homepage.jsx
@@ -9,19 +9,21 @@ const Homepage = () => {
   };
   return (
     <div className="homepage">
-      <div className="title">
-        <h1>Movie Fanatics</h1>
-      </div>
-      <div className="desc">
-        <h2>
-          Discover what's hot and get recommendations<br></br>Connect and
-          discuss your opinions with other users!
-        </h2>
-      </div>
-      <div className="enter">
-        <button className="enter-btn" onClick={handleClick}>
-          Enter
-        </button>
+      <div className="homepage-content">
+        <div className="title">
+          <h1>Movie Fanatics</h1>
+        </div>
+        <div className="desc">
+          <h2>
+            Discover what's hot and get recommendations<br></br>Connect and
+            discuss your opinions with other users!
+          </h2>
+        </div>
+        <div className="enter">
+          <button className="enter-btn" onClick={handleClick}>
+            Enter
+          </button>
+        </div>
       </div>
     </div>
   );
