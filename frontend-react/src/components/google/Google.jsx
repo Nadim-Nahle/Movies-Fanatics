4bf3badf6ae8fafd9f954ea459e0e65bd5f92268
Nadim <nahlenadim@gmail.com>
2022-07-22 10:26:30 +0300

    Frontend-react Create- Google Login

--- frontend-react/src/components/google/Google.jsx
+++ frontend-react/src/components/google/Google.jsx
@@ -1,12 +1,23 @@
 import React, { useEffect } from 'react'
 import jwt_decode from "jwt-decode";
 import axios from 'axios';
+import useAuth from '../../hooks/useAuth';
+import { useLocation, useNavigate } from 'react-router';
+import { useCookies } from 'react-cookie';
 
 const REGISTER_URL = "/api/v1/auth/register/google";
 const LOGIN_URL = "/api/v1/auth/login/google";
 
 
 const Google = () => {
+
+    const { setAuth } = useAuth();
+
+    const navigate = useNavigate();
+    const location = useLocation();
+    const from = location.state?.from?.pathname || "/";
+
+    const [cookies, setCookie, removeCookie] = useCookies(["user"]);
 
 
     function handleCallBackResponse(response){
@@ -15,9 +26,9 @@ const Google = () => {
         var userObject = jwt_decode(response.credential)
 
         localStorage.setItem('fullName',(userObject.name));
-        localStorage.setItem('email',(userObject.email));
-        localStorage.setItem('username',(`${userObject.given_name}_${userObject.family_name}`));
-        localStorage.setItem('url',(userObject.picture));
+        localStorage.setItem('email1',(userObject.email));
+        localStorage.setItem('username1',(`${userObject.given_name}_${userObject.family_name}`));
+        localStorage.setItem('url1',(userObject.picture));
 
         googleRegister();
         
@@ -41,9 +52,9 @@ const Google = () => {
     const googleRegister = async () => {
 
         const name = localStorage.getItem('fullName');
-        const email = localStorage.getItem('email');
-        const username = localStorage.getItem('username');
-        const url = localStorage.getItem('url');
+        const email = localStorage.getItem('email1');
+        const username = localStorage.getItem('username1');
+        const url = localStorage.getItem('url1');
 
         try {
 
@@ -54,9 +65,42 @@ const Google = () => {
                 url
               });
               console.log(response.data)
+              googleLogin()
             
         } catch (error) {
             console.log(error)
+            googleLogin()
+
+        }
+    }
+
+    const googleLogin = async () => {
+        const email = localStorage.getItem('email');
+
+        try {
+            const response = await axios.post(LOGIN_URL, {   
+                email,                     
+              });
+                console.log(response.data)
+                const user = response?.data?.user;
+                const accessToken = response?.data?.secret_token;
+                localStorage.setItem("email", response.data.user.email);
+                localStorage.setItem("userId", response.data.user._id);
+                localStorage.setItem("name", response.data.user.name);
+                localStorage.setItem("url", response.data.url);
+                localStorage.setItem("username", response.data.user.username);
+                localStorage.setItem("AuthToken", accessToken);
+                setCookie("user", response.data.user);
+                setCookie("Email", response.data.user.email);
+                setCookie("userId", response.data.user._id);
+                setCookie("AuthToken", accessToken);
+
+                setAuth({ user });
+                navigate(from, { replace: true });
+
+            
+        } catch (error) {
+            
         }
     }
 
