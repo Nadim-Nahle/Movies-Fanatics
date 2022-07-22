4d61ba8545e34e79c7a97dadb29c497ba73163ed
Nadim <nahlenadim@gmail.com>
2022-07-22 20:45:10 +0300

    Frontend-React Fix- Download API

--- frontend-react/src/components/youtubeDownloader/YoutubeDownloader.jsx
+++ frontend-react/src/components/youtubeDownloader/YoutubeDownloader.jsx
@@ -5,21 +5,20 @@ import './youtubeDownloader.css'
 const YoutubeDownloader = ({videoId}) => {
 
     
-    const options = {
-        method: 'GET',
-        url: 'https://youtube-dl4.p.rapidapi.com/',
-        params: {url: `https://www.youtube.com/watch?v=${videoId}`},
-        headers: {
-          'X-RapidAPI-Key': '1f947199c7msh3bc6fd219522000p1bd19cjsn318d2499f463',
-          'X-RapidAPI-Host': 'youtube-dl4.p.rapidapi.com'
-        }
-      };
+  const options = {
+    method: 'GET',
+    url: `https://youtube-downloader9.p.rapidapi.com/${videoId}/videoandaudio`,
+    headers: {
+      'X-RapidAPI-Key': '1f947199c7msh3bc6fd219522000p1bd19cjsn318d2499f463',
+      'X-RapidAPI-Host': 'youtube-downloader9.p.rapidapi.com'
+    }
+  };
 
     const getDownloads = async () => {
         try {
             const response = await axios.request(options)
             const link =(response.data);
-            window.open(link.url, "_blank")
+            window.open(link[0]?.url, "_blank")
             
             
             
