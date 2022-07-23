201b49d9e269fc59e48bda56892daa43690515b1
Nadim <nahlenadim@gmail.com>
2022-07-23 12:07:54 +0300

    Frontend-React Create- Profile layout

--- frontend-react/src/pages/profile/Profile.jsx
+++ frontend-react/src/pages/profile/Profile.jsx
@@ -8,54 +8,23 @@ const Profile = () => {
   const url = localStorage.getItem("url");
 
   return (
-    <>
-      <div className="wrapper">
-        <div className="left">
-          <img
-            src={
-              url
-                ? url
-                : "https://upload.wikimedia.org/wikipedia/commons/2/24/Missing_avatar.svg"
-            }
-            alt="user"
-            width="100"
-          />
-          <h4>{name}</h4>
-        </div>
-        <div className="right">
-          <div className="info">
-            <h4>
-              <button className="follow-btn">follow</button>
-            </h4>
-            <h3>Information</h3>
-            <div className="info_data">
-              <div className="data">
-                <h4>Email</h4>
-                <p>{email}</p>
-              </div>
-              <div className="data">
-                <h4>username</h4>
-                <p>{username}</p>
-              </div>
-            </div>
-          </div>
-
-          <div className="projects">
-            <h3>activity</h3>
-            <div className="projects_data">
-              <div className="data">
-                <h4>Following</h4>
-                <p>private</p>
-              </div>
-              <div className="data">
-                <h4>Followers</h4>
-                <p>private</p>
-              </div>
-            </div>
-          </div>
-        </div>
+    <div className="profile">
+      <div className="left-profile">
+        left
       </div>
-    </>
+      <div className="right-profile">
+        <form className="profile-form">
+          <div className="left-form">
+            <input type="text" />
+            <input type="text" />
+          </div>
+          <div className="right-form">
+            <input type="text" />
+            <input type="text" />
+          </div>
+        </form>
+      </div>
+    </div>
   );
 };
 
