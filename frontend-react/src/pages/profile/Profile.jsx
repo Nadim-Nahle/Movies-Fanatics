4cfdd382d7a1a1c4831faa24cdb4c1e58a6eb234
Nadim <nahlenadim@gmail.com>
2022-07-23 14:44:23 +0300

    Frontend-React Link- Edit Profile API

--- frontend-react/src/pages/profile/Profile.jsx
+++ frontend-react/src/pages/profile/Profile.jsx
@@ -1,11 +1,34 @@
+import axios from "axios";
 import React, { useState } from "react";
+import { useCookies } from "react-cookie";
 import "./profile.css";
 
 const Profile = () => {
-  const name = localStorage.getItem("name");
-  const username = localStorage.getItem("username");
-  const email = localStorage.getItem("email");
-  const url = localStorage.getItem("url");
+  const [name, setName] = useState(localStorage.getItem('name'));
+  const [username, setUserame] = useState(localStorage.getItem('username'));
+  const [email, setEmail] = useState(localStorage.getItem('email'));
+  const [url, setUrl] = useState(localStorage.getItem('url'));
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [country, setCountry] = useState('');
+  const [city, setCity] = useState('');
+
+  const [cookies] = useCookies(["user"]);
+  const user = cookies?.user;
+  const AuthToken = cookies?.AuthToken;
+
+  
+
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.patch(`/api/v1/auth/user/update/${user._id}`,{name,email,username,country,city,phoneNumber});
+      console.log(response.data)
+    } catch (error) {
+      console.log(error.response)
+      console.log(user._id)
+    }
+  }
 
   return (
     <div className="profile-page">
@@ -29,18 +52,18 @@ const Profile = () => {
    <div class="title_container">
      <h2>Edit Your Profile</h2>
    </div>
-   <form>
+   <form onSubmit={handleSubmit}>
      <div class="row clearfix">
        <div class="col_half">
          <label>Full Name</label>
          <div class="input_field"> <span><i aria-hidden="true" class="fa fa-user"></i></span>
-           <input type="text" name="first_name" placeholder="John" required />
+           <input type="text" name="first_name" placeholder="John" onChange={(e) => setName(e.target.value)}/>
          </div>
        </div>
        <div class="col_half">
          <label>User Name</label>
          <div class="input_field"> <span><i aria-hidden="true" class="fa fa-user"></i></span>
-           <input type="text" name="last_name" placeholder="Doe" />
+           <input type="text" name="last_name" placeholder="Doe" onChange={(e) => setUserame(e.target.value)}/>
          </div>
        </div>
      </div>
@@ -48,13 +71,13 @@ const Profile = () => {
        <div class="col_half">
          <label>Email</label>
          <div class="input_field"> <span><i aria-hidden="true" class="fa fa-envelope"></i></span>
-           <input type="email" name="email" placeholder="johndoe@gmail.com" required />
+           <input type="email" name="email" placeholder="johndoe@gmail.com" onChange={(e) => setEmail(e.target.value)}/>
          </div>
        </div>
        <div class="col_half">
          <label>Phone</label>
          <div class="input_field"> <span><i aria-hidden="true" class="fa fa-phone"></i></span>
-           <input type="tel" name="phone" placeholder="Phone no" pattern="[0-9]{10}" />
+           <input type="tel" name="phone" placeholder="Phone no" pattern="[0-9]{10}" onChange={(e) => setPhoneNumber(e.target.value)}/>
          </div>
        </div>
      </div>
@@ -62,20 +85,20 @@ const Profile = () => {
        <div class="col_half">
          <label>Country</label>
          <div class="input_field"> <span><i aria-hidden="true" class="fa fa-globe"></i></span>
-           <input type="email" name="email" placeholder="johndoe@gmail.com" required />
+           <input type="text" name="email" placeholder="Lebanon" onChange={(e) => setCountry(e.target.value)}/>
          </div>
        </div>
        <div class="col_half">
          <label>City</label>
          <div class="input_field"> <span><i aria-hidden="true" class="fa fa-map-marker"></i></span>
-           <input type="tel" name="phone" placeholder="Phone no" pattern="[0-9]{10}" />
+           <input type="tel" name="phone" placeholder="City" onChange={(e) => setCity(e.target.value)} />
          </div>
        </div>
      </div>
      <div class="row clearfix">
      </div>
      <div className="form-btn">
-        <button className="profile-btn">Save Profile</button>
+        <button className="profile-btn" type="submit">Save Profile</button>
      </div>
      
    </form>
