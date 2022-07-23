8ea5ef18363e70a5ae83d13bb18ad2d957a80475
Nadim <nahlenadim@gmail.com>
2022-07-23 09:24:13 +0300

    Chrome-Extension Insatll- style-loader

--- chrome-extension/webpack.config.js
+++ chrome-extension/webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
   },
   module: {
     rules: [{ 
-        test: /\.(js|jsx)$/, 
+        test: /\.(js|jsx|css)$/, 
         exclude: /node_modules/,
         use: {
             loader: 'babel-loader',
