3ec72694465c369640cfa496170880663fca4661
Nadim <nahlenadim@gmail.com>
2022-07-24 10:36:31 +0300

    Frontend-React Format- Video Stream Page

--- frontend-react/src/components/videoStream/VideoStream.jsx
+++ frontend-react/src/components/videoStream/VideoStream.jsx
@@ -1,15 +1,18 @@
-import React from 'react';
-import './videoStream.css';
+import React from "react";
+import "./videoStream.css";
 
 const VideoStream = () => {
   return (
-    <div className='video-stream'>
-        <div className="video-stream-card">
-            <video className='video-card' src="http://localhost:9000/movie"  controls></video>
-        </div>
-        
+    <div className="video-stream">
+      <div className="video-stream-card">
+        <video
+          className="video-card"
+          src="http://localhost:9000/movie"
+          controls
+        ></video>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default VideoStream;
