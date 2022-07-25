58bd8804e71218d31298fae65df1dc9634e9e91c
Nadim <nahlenadim@gmail.com>
2022-07-25 09:18:08 +0300

    Frontend-React Format- Navbar

--- frontend-react/src/components/navbar/Navbar.jsx
+++ frontend-react/src/components/navbar/Navbar.jsx
@@ -11,89 +11,90 @@ function Navbar() {
   const location = useLocation();
   const { auth } = useAuth();
 
-  
-
   const handleClick = () => setClick(!click);
   const handleLogout = () => {
-    removeCookie('user');
-    setAuth('');
-  }
+    removeCookie("user");
+    setAuth("");
+  };
   return (
     <>
       <nav className="navbar">
         <div className="nav-container">
           <NavLink exact to="/" className="nav-logo">
-            <img className="logo" src={require("../../img/logo.png")} alt='logo' />
+            <img
+              className="logo"
+              src={require("../../img/logo.png")}
+              alt="logo"
+            />
           </NavLink>
 
           <ul className={click ? "nav-menu active" : "nav-menu"}>
-          {location.pathname.includes("auth") && (
-            <li className="nav-item">
-              <NavLink
-                exact
-                to=''
-                activeclassname="active"
-                className="nav-links"
-                onClick={handleLogout}
-              >
-                Logout
-              </NavLink>
-            </li>
-          )}
-            {auth.user ?
-            <li className="nav-item">
-              <NavLink
-                exact
-                to="/auth/profile"
-                activeclassname="active"
-                className="nav-links"
-                onClick={handleClick}
-              >
-                Profile
-              </NavLink>
-            </li>
-            :null}
-            {auth.user ?
-            <li className="nav-item">
-              <NavLink
-                exact
-                to="/auth/messenger"
-                activeclassname="active"
-                className="nav-links"
-                onClick={handleClick}
-              >
-                Messenger
-              </NavLink>
-            </li>
-            :null
-            }
-            {auth.user ?
-            <li className="nav-item">
-              <NavLink
-                exact
-                to="/auth/swipe"
-                activeclassname="active"
-                className="nav-links"
-                onClick={handleClick}
-              >
-                Connect
-              </NavLink>
-            </li>
-            :null}
-            {auth.user ?
-            <li className="nav-item">
-              <NavLink
-                exact
-                to="/auth/premium"
-                activeclassname="active"
-                className="nav-links"
-                onClick={handleClick}
-              >
-                Premuim
-              </NavLink>
-            </li>
-            :null}
-            
+            {location.pathname.includes("auth") && (
+              <li className="nav-item">
+                <NavLink
+                  exact
+                  to=""
+                  activeclassname="active"
+                  className="nav-links"
+                  onClick={handleLogout}
+                >
+                  Logout
+                </NavLink>
+              </li>
+            )}
+            {auth.user ? (
+              <li className="nav-item">
+                <NavLink
+                  exact
+                  to="/auth/profile"
+                  activeclassname="active"
+                  className="nav-links"
+                  onClick={handleClick}
+                >
+                  Profile
+                </NavLink>
+              </li>
+            ) : null}
+            {auth.user ? (
+              <li className="nav-item">
+                <NavLink
+                  exact
+                  to="/auth/messenger"
+                  activeclassname="active"
+                  className="nav-links"
+                  onClick={handleClick}
+                >
+                  Messenger
+                </NavLink>
+              </li>
+            ) : null}
+            {auth.user ? (
+              <li className="nav-item">
+                <NavLink
+                  exact
+                  to="/auth/swipe"
+                  activeclassname="active"
+                  className="nav-links"
+                  onClick={handleClick}
+                >
+                  Connect
+                </NavLink>
+              </li>
+            ) : null}
+            {auth.user ? (
+              <li className="nav-item">
+                <NavLink
+                  exact
+                  to="/auth/premium"
+                  activeclassname="active"
+                  className="nav-links"
+                  onClick={handleClick}
+                >
+                  Premuim
+                </NavLink>
+              </li>
+            ) : null}
+
             <li className="nav-item">
               <NavLink
                 exact
@@ -105,34 +106,33 @@ function Navbar() {
                 Movies
               </NavLink>
             </li>
-            
-            
-            {auth.user ? null :
-            <li className="nav-item">
-              <NavLink
-                exact
-                to="/register"
-                activeclassname="active"
-                className="nav-links"
-                onClick={handleClick}
-              >
-                Sign up
-              </NavLink>
-            </li>
-            }
-            {auth.user ? null :
-            <li className="nav-item">
-              <NavLink
-                exact
-                to="/login"
-                activeclassname="active"
-                className="nav-links"
-                onClick={handleClick}
-              >
-                Sign in
-              </NavLink>
-            </li>
-            }
+
+            {auth.user ? null : (
+              <li className="nav-item">
+                <NavLink
+                  exact
+                  to="/register"
+                  activeclassname="active"
+                  className="nav-links"
+                  onClick={handleClick}
+                >
+                  Sign up
+                </NavLink>
+              </li>
+            )}
+            {auth.user ? null : (
+              <li className="nav-item">
+                <NavLink
+                  exact
+                  to="/login"
+                  activeclassname="active"
+                  className="nav-links"
+                  onClick={handleClick}
+                >
+                  Sign in
+                </NavLink>
+              </li>
+            )}
           </ul>
           <div className="nav-icon" onClick={handleClick}>
             <i className={click ? "fas fa-times" : "fas fa-bars"}></i>
