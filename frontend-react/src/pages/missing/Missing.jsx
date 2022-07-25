a4783c370495d7b9a4abb88f4d0716742a3b4a28
Nadim <nahlenadim@gmail.com>
2022-07-25 10:40:59 +0300

    Frontend-React Format- Missing Page

--- frontend-react/src/pages/missing/Missing.jsx
+++ frontend-react/src/pages/missing/Missing.jsx
@@ -6,15 +6,14 @@ const Missing = () => {
   return (
     <div className="missing-page">
       <div className="center-missing">
-      <img className="missing-img" src={miss} alt="miss" />
-      <h1 className="missing-h1">404 Page Does Not Exist!</h1>
-      <h3 className="missing-h3">
-        {" "}
-        The link may be broken, or the page may have been removed. Check to see
-        if the link you're trying to open is correct.
-      </h3>
+        <img className="missing-img" src={miss} alt="miss" />
+        <h1 className="missing-h1">404 Page Does Not Exist!</h1>
+        <h3 className="missing-h3">
+          {" "}
+          The link may be broken, or the page may have been removed. Check to
+          see if the link you're trying to open is correct.
+        </h3>
       </div>
-      
     </div>
   );
 };
